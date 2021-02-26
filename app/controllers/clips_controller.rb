215f640d3585c5e31919b8ae1ebd5069f6dacc77
Rinkashiki <55148458+Rinkashiki@users.noreply.github.com>
2021-02-26 15:01:51 +0100

    Comprobación vídeo adjunto

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -15,6 +15,10 @@ class ClipsController < ApplicationController
   end
 
   def create  
+    if params[ :clip][ :video].present?
+      flash[ :alert] = 'Video not attached'
+      redirect_to new_clip_path
+    end
     @clip = Clip.new clip_params
     @clip[ :clipName] = File.basename(params[ :clip][ :video].original_filename, '.mp4').truncate(20)
     @clip[ :uploadUser] = helpers.current_user[ :name]
