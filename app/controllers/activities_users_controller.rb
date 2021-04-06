c8f5f2c12d9c4bde50f77c12016f30294873b662
Rinkashiki <mariomarquezb98@gmail.com>
2021-04-06 15:06:21 +0200

    Almacenamiento de respuestas de usuario

--- app/controllers/activities_users_controller.rb
+++ app/controllers/activities_users_controller.rb
@@ -4,7 +4,7 @@ class ActivitiesUsersController < ApplicationController
     layout 'in_session', only: [ :index]
 
     # Extract the current activity before any method is executed
-    before_action :set_activity, only: [ :doing_activity]
+    before_action :set_activity, only: [ :doing_activity, finish_activity]
 
     def index
         query = "SELECT a.* from activities a JOIN activities_users au ON a.id = au.activity_id 
@@ -35,6 +35,44 @@ class ActivitiesUsersController < ApplicationController
         if !@question.clip_id.nil?
           @clip = Clip.find(@question.clip_id)
         end
+
+        # Save answers
+        if @n_question > 0
+
+          # Extract the answers from activity_user 
+          query = "SELECT answers from activities_users WHERE activity_id = '#{@activity[ :id]}' 
+          AND user_id = '#{helpers.current_user[ :id]}'"
+
+          @user_answers = ActiveRecord::Base.connection.exec_query(query)
+
+          if params[ :user_answer].nil?
+            @users_answers.push("nil")
+          else
+           @users_answers.push(params[ :user_answer])
+          end
+
+          # Update answers in DB
+          query = "UPDATE activities_users SET answers = '#{@user_answers}' WHERE activity_id = '#{@activity[ :id]}' 
+          AND user_id = '#{helpers.current_user[ :id]}'"
+
+          ActiveRecord::Base.connection.exec_query(query)
+        end
+
+      end
+
+      def finish_activity
+
+        # Extract questions
+        query = "SELECT q.* from questions q JOIN activities_questions aq ON q.id = aq.question_id 
+        where aq.activity_id = '#{@activity[ :id]}'"
+        
+        @questions = Question.find_by_sql(query)
+
+        #Extract user_answers
+        query = "SELECT answers from activities_users WHERE activity_id = '#{@activity[ :id]}' 
+        AND user_id = '#{helpers.current_user[ :id]}'"
+
+        @user_answers = ActiveRecord::Base.connection.exec_query(query)
 
       end
 
